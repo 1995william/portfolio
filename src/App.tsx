a39e3868abdbe886f652598418d147bac3e462b2
unknown <rwps1234@gmail.com>
2022-06-14 22:57:45 -0300

    importando header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import Header from "./Components/Header/Header";
 import Hexagon from "./Components/Hexagon/Hexagon";
+import Icons from "./Components/Particles/Particles";
 import styles from "./styles/App.module.css";
 
 function App() {
   return (
     <>
-      <Hexagon />
       <div className={styles.container__app}>
-        
+      <Hexagon />
+      <Icons/>
+      <Header/>
       </div>
     </>
   );
