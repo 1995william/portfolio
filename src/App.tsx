ac8235f3617ba6777901d433cd497b9aceca642e
unknown <rwps1234@gmail.com>
2022-06-19 22:37:25 -0300

    importando sobre ao app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import BemVindo from "./Components/BemVindo/BemVindo";
 import Header from "./Components/Header/Header";
 import Hexagon from "./Components/Hexagon/Hexagon";
 import Icons from "./Components/Particles/Particles";
+import Sobre from "./Components/Sobre/Sobre";
 import styles from "./styles/App.module.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Header/>
       <main className={styles.main}>
         <BemVindo/>
+        <Sobre/>
       </main>
       </div>
     </>
