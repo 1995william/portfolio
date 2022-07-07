26c627a67a76b56e3617525fb71472f8082c0735
Will <rwps1234@gmail.com>
2022-07-07 00:17:07 -0300

    adicionando icones de redes sociais

--- src/Components/Contato/Contato.tsx
+++ src/Components/Contato/Contato.tsx
@@ -1,4 +1,5 @@
-import { FiSend } from "react-icons/fi";
+import { FiSend, FiMail } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import styles from "./Contato.module.scss";
 import email from "../../assets/imgs/email.png";
 import ScrollAnimation from "react-animate-on-scroll";
@@ -7,6 +8,7 @@ import { useForm } from "react-hook-form";
 import { validationSchema } from "../../utils/validation";
 import { useState,useRef } from "react";
 import { sendEmail } from "../../service/sendEmail";
+import Botao from "../Botao/Botao";
 
 export default function Contato() {
   const [formOk, setFormOK] = useState(false);
@@ -21,9 +23,9 @@ export default function Contato() {
   }
 
    const onSubmit = () => {   
-    setFormOK(true);
-    sendEmail(form.current);
- 
+     setFormOK(true);
+     sendEmail(form.current);
+     form.current.reset();
    }
    return (
     <section className={styles.container__contato} id="contato">
@@ -44,7 +46,7 @@ export default function Contato() {
       <div className={styles.container__formulario}>
         <ScrollAnimation
           animateIn="animate__fadeInLeft"
-          delay={0.6 * 1000}
+          delay={0.4 * 1000}
         >
 
         <form ref={form} onSubmit={handleSubmit(onSubmit)} className={styles.formulario}>
@@ -57,9 +59,9 @@ export default function Contato() {
               {...register("nome")}
               onKeyUp={verificaCampos}
               placeholder="Nome completo"
-              className={`${errors.nome != undefined ? styles.campoInvalido:''}` }
+              className={`${errors.nome != undefined && styles.campoInvalido}` }
             />
-            <span className={`${errors.nome != undefined ? styles.mensagemInvalida:''}` }>{errors.nome?.message ? errors.nome?.message?.toString(): ''}</span>
+            <span className={`${errors.nome != undefined && styles.mensagemInvalida}` }>{errors.nome?.message ? errors.nome?.message?.toString(): ''}</span>
           </label>
       
           <label>
@@ -70,9 +72,9 @@ export default function Contato() {
               placeholder="Digite seu melhor e-mail"
               {...register("email")}
               onKeyUp={verificaCampos}
-              className={`${errors.email != undefined ? styles.campoInvalido:''}` }
+              className={`${errors.email != undefined && styles.campoInvalido}` }
             />
-            <span className={`${errors.email != undefined ? styles.mensagemInvalida:''}` }>{errors.email?.message ? errors.email?.message?.toString(): ''}</span>
+            <span className={`${errors.email != undefined && styles.mensagemInvalida}` }>{errors.email?.message ? errors.email?.message?.toString(): ''}</span>
           </label>
 
           <label>
@@ -84,30 +86,43 @@ export default function Contato() {
               cols={20}
               {...register('mensagem')}
              onKeyUp={verificaCampos}
-              className={`${errors.mensagem != undefined ? styles.campoInvalido:''}` }
+              className={`${errors.mensagem != undefined && styles.campoInvalido}` }
             ></textarea>
             <div>
 
-            <span className={`${errors.mensagem != undefined ? styles.mensagemInvalida:''}`}>{errors.mensagem?.message ? errors.mensagem?.message?.toString() : ''}</span>
+            <span className={`${errors.mensagem != undefined && styles.mensagemInvalida}`}>{errors.mensagem?.message && errors.mensagem?.message?.toString()}</span>
             <span className={`${formOk ? styles.mensagemSucesso: styles.invisivel}`}>Mensagem enviada com sucesso</span>
             </div>
           </label>
-
-          <div className={styles.container__botao}>
-            <button onClick={verificaCampos}>
-              <FiSend />
-              Enviar mensagem
-            </button>
-          </div>
+          
+          <Botao
+          onClick={verificaCampos}
+          icone={<FiSend size={'20px'}/>}
+          texto="Enviar Mensagem"
+          />
         </form>
         </ScrollAnimation>
       </div>
+    <div className={styles.container__email_redeSociais}>
 
       <figure className={styles.container__emailLogo}>
-        <ScrollAnimation animateIn="animate__fadeInRight" delay={0.6 * 1000}>
+        <ScrollAnimation animateIn="animate__fadeInRight" delay={0.4 * 1000}>
           <img src={email} alt="email" />
         </ScrollAnimation>
       </figure>
+      <ScrollAnimation animateIn="animate__fadeInRight" delay={0.2 * 1000}>
+      <p>Me siga nas redes sociais</p>
+      </ScrollAnimation>
+
+      <figure className={styles.container__redeSociais}>
+        <ScrollAnimation animateIn="animate__fadeInRight" delay={0.4 * 1000}>
+          <a href="https://github.com/1995william" target="_blank"><FaGithub size={"40px"}/></a> 
+          <a href="https://www.linkedin.com/in/rodrigo-william1/" target="_blank"><FaLinkedin size={"40px"}/></a> 
+          <a href="mailto:rwps1234@gmail.com" target="_blank"><FiMail size={"40px"}/></a> 
+          
+        </ScrollAnimation>
+      </figure>
+    </div>
     </section>
   );
 }
